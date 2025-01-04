b689d95f0611496435e09910fd592fa67f8b4259
Nimira43 <n.rabone72@aol.com>
2025-01-04 09:42:23 +0000

    Starting to write the User schema for the Database

---
+++ backend/model/User/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const crypto = require(crypto)
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    required: [true, 'Please enter your email address.'],
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter your password.'],
+    minLength: [8, "Password must be at least 8 characters long."]
+  },
+  firstName: {
+    type: String,
+    required: [true, 'Please enter your first name.'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Please enter your last name.'],
+  },
+  strip_customer_id: {
+    type: String,
+  },
+  subscriptions: [],
+  isVerfied: {
+    type: Boolean,
+    default: false,
+  },
+  accountVerificationToken: String,
+  accountVerificationTokenExpires: Date,
+  passwordChangeAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  profilePhoto: {
+    type: String,
+    // default: "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_960_720.png",
+    default: "../../public/user.png",
+  },
+}, {
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true},
+  timeStamps: true,
+}) 
