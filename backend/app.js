99d78e39ad9cf9fa11191ea4b64b51a0ad920c06
Nimira43 <n.rabone72@aol.com>
2025-01-04 08:30:37 +0000

    Starting to set up MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 5000
 
 app.get('/', (req, res) => {
   res.send('Praesidium')
 })
 
-app.listen(PORT, () => {
-  console.log(`Servers is listening on Port: ${PORT}`)
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is listening on Port: ${PORT}`)
 })
